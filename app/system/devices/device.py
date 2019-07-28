aefd0c302186efc49dce527a527ba4a0c6823d25
Kostiantyn Kostiuk <kostyanf14@live.com>
2019-07-28 11:24:12 +0300

    Исправлено форматирование в соответствии с PEP8
    Задача: #9

--- app/system/devices/device.py
+++ app/system/devices/device.py
@@ -1,10 +1,11 @@
 from abc import ABC, abstractmethod
 
+
 class Device(ABC):
     @abstractmethod
     def start(self):
-    	pass
+        pass
 
     @abstractmethod
     def stop(self):
-    	pass
+        pass
--- app/system/devices/gpio.py
+++ app/system/devices/gpio.py
@@ -1,24 +1,25 @@
 from os import path
 from . import Device
 
+
 class GPIO(Device):
     INIT_PATH = '/sys/class/gpio/export'
     PORT_PATH = '/sys/class/gpio/gpio{}'
 
     def __init__(self, port):
-    	self.path = self.PORT_PATH.format(port)
-    	if not path.exists(self.path):
-    		self.__write_to_file__(self.INIT_PATH, str(port))
-    		self.__write_param__('direction', 'out')
+        self.path = self.PORT_PATH.format(port)
+        if not path.exists(self.path):
+            self.__write_to_file__(self.INIT_PATH, str(port))
+            self.__write_param__('direction', 'out')
 
     def start(self):
-    	self.__write_param__('value', '1')
+        self.__write_param__('value', '1')
 
     def stop(self):
-    	self.__write_param__('value', '0')
+        self.__write_param__('value', '0')
 
     def __write_param__(self, param, value):
-    	self.__write_to_file__('{}/{}'.format(self.path, param), value)
+        self.__write_to_file__('{}/{}'.format(self.path, param), value)
 
     @staticmethod
     def __write_to_file__(filename, data):
