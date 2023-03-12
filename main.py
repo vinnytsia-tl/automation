980b1cac8a49f916f306248e52be9ccb9e3bb9e1
Just Me <just806me@gmail.com>
2023-03-12 18:49:58 +0200

    Add minimum version requirement

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import sys
+
+if sys.version_info < (3, 10):
+    sys.exit('Python 3.10 or higher is required')
+
+# pylint: disable=wrong-import-position
 from app.web import Web
 from app.config import Config
 from app.database.migrations import apply_migrations
 from app.logging import setup_app_logger
+# pylint: enable=wrong-import-position
 
 
 if __name__ == '__main__':
--- rule_scheduler.py
+++ rule_scheduler.py
@@ -1,6 +1,13 @@
+import sys
+
+if sys.version_info < (3, 10):
+    sys.exit('Python 3.10 or higher is required')
+
+# pylint: disable=wrong-import-position
 from app.config import Config
 from app.service import RuleScheduler
 from app.logging import setup_app_logger
+# pylint: enable=wrong-import-position
 
 
 if __name__ == '__main__':
