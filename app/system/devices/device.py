de524fd66491e556a323250911db719f63aae37b
Just Me <just806me@gmail.com>
2023-07-16 20:42:34 +0300

    Add logging to device handler run

--- app/system/devices/device.py
+++ app/system/devices/device.py
@@ -29,12 +29,24 @@ class Device:
         return 0
 
     def run(self, duration: int) -> None:
+        logger.info('Running %s for %d seconds', repr(self), duration)
+
         event_loop = asyncio.get_event_loop()
         stop_time = event_loop.time() + duration - self.stop_delay
 
         if self.stop_handle is None:
+            logger.debug('Device not running, starting it')
             event_loop.call_soon(self.start)
+            logger.debug('Scheduling device to stop at %d', stop_time)
             self.stop_handle = event_loop.call_at(stop_time, self.stop)
-        elif self.stop_handle.when() < stop_time:
+            return
+
+        scheduled_stop_time = self.stop_handle.when()
+        logger.debug('Device already running, skipping start. Scheduled stop time is %d', scheduled_stop_time)
+
+        if scheduled_stop_time < stop_time:
+            logger.debug('Extending run time till %d', stop_time)
             self.stop_handle.cancel()
             self.stop_handle = event_loop.call_at(stop_time, self.stop)
+        else:
+            logger.debug('Run time is already sufficient')
